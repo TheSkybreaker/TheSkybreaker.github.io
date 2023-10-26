25d0f342836c765c3b0af11768aaf5f414639af4
TheSkybreaker <simoni.itaer@gmail.com>
2023-10-26 17:47:22 +0200

    inizio animazione accensione pc

---
+++ openingAnimation/assets/js/animatePC.js
@@ -0,0 +1,25 @@
+window.onload = () => {
+  const centerDiv = document.querySelector("div#centerDiv");
+  const topDiv = document.querySelector("div#topDiv");
+  const bottomDiv = document.querySelector("div#bottomDiv");
+
+  centerDiv.animate([{ width: "100%", left: "0%" }], {
+    duration: 700,
+    fill: "forwards",
+  }).onfinish = () => {
+    bottomDiv.animate([{ height: "0%", opacity: 0 }], {
+      duration: 400,
+      fill: "forwards",
+    });
+
+    topDiv.animate([{ height: "0%", opacity: 0 }], {
+      duration: 400,
+      fill: "forwards",
+    }).onfinish = () => {
+      centerDiv.animate([{ opacity: 0 }], {
+        duration: 200,
+        fill: "forwards",
+      });
+    };
+  };
+};
